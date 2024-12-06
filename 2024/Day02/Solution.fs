bc45e9056ea459747e320569cb82fe5b2cee90ec
Mahdi Hasnat Siyam <mahdibuet3@gmail.com>
2024-12-07 01:30:42 +0600

    complete part1 day 02

---
+++ 2024/Day02/Solution.fs
@@ -0,0 +1,35 @@
+module Day02
+
+open System.IO
+
+
+let readInputs () : array<array<int>> =
+    let reader = File.OpenText ("Day02/input.txt")
+    seq{
+        while not reader.EndOfStream do
+            let line = reader.ReadLine ()
+            let parts = line.Split(" ");
+            yield
+                parts
+                |> Array.map int
+    }
+    |> Seq.toArray
+
+let countSafe () : int =
+    let inputs = readInputs ()
+    inputs
+    |> Array.sumBy (fun arr ->
+        arr
+        |> Array.pairwise
+        |> Array.map (fun (a, b) ->
+            let diff = abs (a - b)
+            1 <= diff && diff <= 3, a < b
+        )
+        |> fun arr ->
+            printfn $"%A{arr}"
+            (arr |> Array.map fst |> Array.forall id)
+            && (arr |> Array.map snd |> Set.ofArray |> Set.count = 1)
+        |> function
+            | true -> 1
+            | false -> 0
+    )
--- 2024/Tests.fs
+++ 2024/Tests.fs
@@ -10,3 +10,7 @@ type Tests(output: ITestOutputHelper) =
         output.WriteLine($"SumOfSortedAbsDiff: {Day01.sumOfSortedAbsDiff ()}")
         output.WriteLine($"SimilarityScore: {Day01.similarityScore ()}")
         Assert.True(true)
+
+    [<Fact>]
+    let ``Day 02`` () =
+        output.WriteLine($"Count: {Day02.countSafe ()}");
